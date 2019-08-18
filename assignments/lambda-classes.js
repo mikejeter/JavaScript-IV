593458224c1f157a0c601d6c006b24ec96a7c66f
mikejeter <mjeter45@yahoo.com>
2019-08-18 11:26:25 -0400

    modified prototype-refactor.js, lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attrs) {
+        this.name = name.attrs;
+        this.age = age.attrs;
+        this.location = location.attrs; 
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name} I am from ${this.location}`);
+    }
+}
+
